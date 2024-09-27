7f7dba21be4133e46e93918e583712eaffb890f6
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-09-27 16:56:33 -0300

    feat(home): finaliza implementação da página inicial
    - Adiciona componentes principais
    - Implementa layout responsivo
    - Integra dados dinâmicos
    - Otimiza performance de carregamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import heroSvg from "@/assets/hero.svg"
+
 export default function Home() {
   return (
-    <main>
-
+    <main className="flex min-h-[calc(100vh-80px)] items-center justify-center flex-col" >
+      <h1 className="font-medium text-2xl mb-2" >Gerencie sua empresa</h1>
+      <h2 className="font-bold text-3xl md:text-4xl mb-8 text-blue-500" >Atendimento e clientes</h2>
+      <Image
+        src={heroSvg}
+        alt="Imagem de um rapaz de suporte do cliente"
+        width={300}
+        className="max-w-sm md:max-w-xl"
+      />
     </main>
   );
 }
