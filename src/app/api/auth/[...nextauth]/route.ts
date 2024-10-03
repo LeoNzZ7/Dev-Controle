a9974f26bfeee63623542add3b6c8a186e2883ec
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-10-03 19:53:22 -0300

    feat: Integração da biblioteca HeadlessUI e melhorias na autenticação
    - Adicionada a biblioteca HeadlessUI para componentes de interface
    - Refatorado o componente Header para melhor usabilidade
    - Implementadas funções de login e logout
      - Adicionada função handleLogin para processar o login do usuário
      - Adicionada função handleLogout para processar o logout do usuário
    - Atualizada a lógica de autenticação no componente Header

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST}
-=======
 import { authOptions } from "@/app/lib/auth";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 
 export {  handler as GET, handler as POST }
->>>>>>> 6034192b79230ed04b8df66e502a52485ecea27e
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 "use client"
 
 import Link from "next/link"
-import { FiLogOut, FiUser } from "react-icons/fi"
-import { signIn } from "next-auth/react"
+import { FiLoader, FiLogIn, FiLogOut, FiUser } from "react-icons/fi"
+import { signIn, signOut, useSession } from "next-auth/react"
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react"
+import { MdDashboard } from "react-icons/md"
 
 export const Header = () => {
+    const { status } = useSession()
+
     async function handleLogin() {
         await signIn()
+    }
+
+    async function handleLogout() {
+        await signOut()
     }
 
     return (
@@ -17,13 +25,49 @@ export const Header = () => {
                         <span className="text-blue-500" >dev</span> controle
                     </h1>
                 </Link>
-                <nav className="flex items-baseline gap-4" >
-                    <Link href="/dashboard" >
-                        <FiUser size={24} color="#4B5633" />
-                    </Link>
-                    <button onClick={handleLogin} >
-                        <FiLogOut size={24} color="#4b5633" />
-                    </button>
+                <nav>
+                    {status === 'loading' && (
+                        <FiLoader size={28} color="#4b5563" className="animate-spin" />
+                    )}
+                    {status === 'unauthenticated' && (
+                        <button onClick={handleLogin} >
+                            <FiLogIn size={28} color="#4b5563" />
+                        </button>
+                    )}
+                    {status === "authenticated" && (
+                        <Menu >
+                            <MenuButton>
+                                <FiUser size={28} color="#4b5563" />
+                            </MenuButton>
+                            <MenuItems
+                                anchor="bottom"
+                                className="bg-slate-100 w-[160px] rounded-[6px] flex items-center justify-center flex-col mt-1 drop-shadow border border-slate-300"
+                            >
+                                <MenuItem>
+                                    <Link
+                                        href="/dashboard"
+                                        className="flex justify-between w-full hover:bg-slate-200 px-1 py-2 transition-colors border-b border-slate-200">
+                                        Painel
+                                        <MdDashboard
+                                            color="#4b5563"
+                                            size={24}
+                                        />
+                                    </Link>
+                                </MenuItem>
+                                <MenuItem>
+                                    <button
+                                        onClick={handleLogout}
+                                        className="flex justify-between w-full hover:bg-slate-200 px-1 py-2 transition-colors">
+                                        Sair
+                                        <FiLogOut
+                                            color="#4b5563"
+                                            size={24}
+                                        />
+                                    </button>
+                                </MenuItem>
+                            </MenuItems>
+                        </Menu>
+                    )}
                 </nav>
             </div>
         </header>
